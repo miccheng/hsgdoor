3cc6209d5bd97ad7c897ff98e2e9a4211302a42a
michael.cheng <mcheng.work@gmail.com>
2013-03-19 19:47:15 +0800

    Wrong order or args passed in. *doh*

--- src/funcs.php
+++ src/funcs.php
@@ -45,7 +45,7 @@ function saveAuthCookie($user=array())
     if (empty($user)) return;
     $user['date'] = date('Y-m-d H:i:s');
     $cookie_var = md5(json_encode($user) . getenv('AUTH_COOKIE_SALT'));
-    file_put_contents(json_encode($user), dirname(__DIR__) . '/codes/auth/' . $cookie_var);
+    file_put_contents(dirname(__DIR__) . '/codes/auth/' . $cookie_var, json_encode($user));
     setCookie('hsgdoor_auth', $cookie_var, time() + 1576800000); // 50 years
 }
 
